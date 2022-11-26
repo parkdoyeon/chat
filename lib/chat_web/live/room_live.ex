76a118fd5c38e00416919f2dd15cce1a581f52e3
parkdoyeon <>
2022-11-27 01:25:17 +0900

    Show each user's name with messages

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -6,34 +6,34 @@ defmodule ChatWeb.RoomLive do
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
+    user_name = MnemonicSlugs.generate_slug(2)
     if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
 
     {:ok,
      assign(socket,
        room_id: room_id,
+       user_name: user_name,
        topic: topic,
        message: "",
-       messages: [%{uuid: UUID.uuid4(), content: "Hello"}],
+       messages: [%{uuid: UUID.uuid4(), user_name: "system", content: "#{user_name} joined the chat"}],
        temporary_assigns: [messages: []]
      )}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
-    Logger.info(message: message)
+    message = %{uuid: UUID.uuid4(), content: message, user_name: socket.assigns.user_name}
     ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, assign(socket, message: "")}
   end
 
   @impl true
   def handle_event("form_updated", %{"chat" => %{"message" => message}}, socket) do
-    Logger.info(message: message)
     {:noreply, assign(socket, message: message)}
   end
 
   @impl true
   def handle_info(%{event: "new-message", payload: message, topic: topic}, socket) do
-    Logger.info(payload: message)
-    {:noreply, assign(socket, messages: [%{uuid: UUID.uuid4(), content: message}])}
+    {:noreply, assign(socket, messages: [message])}
   end
 end
